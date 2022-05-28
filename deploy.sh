efa3cf93cbf6606e53a7b542479455a67f1071f5
lzhbhlrPython <Liubaolin20070125@outlook.com>
2022-05-28 19:02:20 +0800

    FKU

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,13 @@
 git pull
 
-git add .
+git add ./static/*
+git add ./template.html
+git add ./github/workflows/*
+git add ./html.py
+git add ./download/*
+git add ./.vscode/*
+git add ./README.md
+git add ./LICENSE
 
 git commit -m "Deploy"
 git push
